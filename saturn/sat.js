9efc82052a2a8826e684861b1d40413b7a686891
Nuttapat Kirawittaya <Nuttapat Kirawittaya>
2016-12-15 10:07:06 +0700

    saturn new version

--- saturn/sat.js
+++ saturn/sat.js
@@ -31,20 +31,21 @@ window.addEventListener( 'load', function() {
   scene.add(sphere);
   rings = createRings(radius, segments);
 
-  scene.add(rings);
+
   stars = createStars(90, 64);
   scene.add(stars);
 
   asteroidlist = createAsteroid();
   setTimeout(function () {
     for(var i = 0 ; i < 300 ; i++)
-      scene.add(asteroidlist[i])
+    rings.add(asteroidlist[i])
   }, 2000);
 
   controls = new THREE.TrackballControls(camera);
   container.appendChild( renderer.domElement );
 
   setTimeout(function () {
+    scene.add(rings);
     render();
   }, 2000);
 
@@ -53,10 +54,12 @@ window.addEventListener( 'load', function() {
 function render() {
   controls.update();
   sphere.rotation.y += 0.005;
-  rings.rotation.y += 0.005;
+  rings.rotation.y += 0.002;
+
   for(var i = 0 ; i < 300 ; i++){
-      asteroidlist[i].rotation.y += (0.2*(Math.PI / 180));
-      asteroidlist[i].rotation.y %= 360;
+    var r = Math.sqrt(asteroidlist[i].position.x*asteroidlist[i].position.x+asteroidlist[i].position.z*asteroidlist[i].position.z);
+    asteroidlist[i].rotation.y += (0.2*(Math.PI / 180));
+    asteroidlist[i].rotation.x -= (0.09*(Math.PI / 180));
   }
   requestAnimationFrame(render);
   renderer.render(scene, camera);
@@ -73,9 +76,10 @@ function createSphere(radius, segments) {
 function createRings(radius, segments) {
   return new THREE.Mesh(new THREE.XRingGeometry(1.2 * radius, 2 * radius, 2 * segments, 5, 0, Math.PI * 2), new THREE.MeshBasicMaterial({
     map: THREE.ImageUtils.loadTexture('saturn-rings.png'),
+    alphaMap: THREE.ImageUtils.loadTexture('saturnringpattern.gif'),
     side: THREE.DoubleSide,
     transparent: true,
-    opacity: 0.5
+    opacity: 0.7
   }));
 }
 
@@ -88,27 +92,34 @@ function createStars(radius, segments) {
 
 function createAsteroid(){
   var list = [];
+  var mesh;
   var loader = new THREE.OBJLoader();
   for(var i = 0 ; i < 300 ; i++){
     var typast = Math.floor((Math.random(38) * 4) + 1);;
     loader.load('ast/'+ typast +'.txt',	function ( object ) {
       object.traverse( function ( child ) {
-  						if ( child instanceof THREE.Mesh ) {
-  							child.material.map = THREE.ImageUtils.loadTexture('asteroid_texture.jpg');
-                var r = 0.58 + Math.random(50)*0.3;
-                var x = Math.random(100)*r;
-                var y = Math.sqrt(r*r-x*x);
-                if(Math.random(70) < 0.5) x = -x;
-                if(Math.random(30) < 0.5) y = -y;
-                z = 0.015 + Math.random()*0.03;
-                if(Math.random(99) < 0.5) z = -z;
-                child.position.set(x,z,y);
-                var size = Math.random(22) * 0.015;
-                if(Math.random(101) < 0.5) size += 0.01
-                child.scale.set(size,size,size);
-            }});
-        list.push(object);
-    });
+        if ( child instanceof THREE.Mesh ) {
+          var material =  new THREE.MeshPhongMaterial({color: 0x616A6B, shading: THREE.SmoothShading});
+          var geometry = new THREE.Geometry().fromBufferGeometry( child.geometry );
+          geometry.computeFaceNormals();
+          geometry.mergeVertices();
+          geometry.computeVertexNormals();
+          geometry.computeBoundingBox();
+          mesh = new THREE.Mesh(geometry,material);
+        }});
+        var r = 0.58 + Math.random(50)*0.3;
+        var x = Math.random(100)*r;
+        var y = Math.sqrt(r*r-x*x);
+        if(Math.random(70) < 0.5) x = -x;
+        if(Math.random(30) < 0.5) y = -y;
+        z = 0.015 + Math.random()*0.03;
+        if(Math.random(99) < 0.5) z = -z;
+        mesh.position.set(x,z,y);
+        var size = Math.random(22) * 0.015;
+        if(Math.random(101) < 0.5) size += 0.01
+        mesh.scale.set(size,size,size);
+        list.push(mesh);
+      });
+    }
+    return list;
   }
-  return list;
-}
